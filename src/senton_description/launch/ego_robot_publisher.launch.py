335523ca0f9b8c253eda26fe451b4f5d895dc191
happy-tang <995210764@qq.com>
2024-09-15 18:53:00 +0800

    [0]添加仿真模型

---
+++ src/senton_description/launch/ego_robot_publisher.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+    pkg_path = os.path.dirname(__file__)
+    xacro_file = os.path.join(pkg_path, 'ego_racecar.xacro')
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='ego_robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro ', xacro_file])
+            }],
+            remappings=[('/robot_description', 'ego_robot_description')]
+        ),
+        Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_baselink_to_laser',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'ego_racecar/front_right_hinge', 'ego_racecar/front_right_wheel']
+        ),
+        Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_baselink_to_laser',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'ego_racecar/front_left_hinge', 'ego_racecar/front_left_wheel']
+        ),
+        
+    ])
---
+++ src/senton_description/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'senton_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tang',
+    maintainer_email='995210764@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
